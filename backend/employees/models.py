82cf4ad58064a56b74b10bd1f1249c2dd46ebd03
Umesh Kumar Rai <89653715+umeshrai01@users.noreply.github.com>
2025-01-02 14:19:03 +0530

    Updated readme

--- backend/employees/models.py
+++ backend/employees/models.py
@@ -6,14 +6,13 @@ class Employee(models.Model):
     dob = models.DateField()
     gender = models.CharField(max_length=10)
     department = models.CharField(max_length=100)
-    age = models.PositiveIntegerField(editable=False)  # age is no longer directly editable
+    age = models.PositiveIntegerField(editable=False)
 
     def __str__(self):
         return self.name
 
     def save(self, *args, **kwargs):
-        # Automatically calculate the age based on the date of birth (dob)
         if self.dob:
             today = date.today()
             self.age = today.year - self.dob.year - ((today.month, today.day) < (self.dob.month, self.dob.day))
-        super(Employee, self).save(*args, **kwargs)  # Call the parent class save method
+        super(Employee, self).save(*args, **kwargs)
